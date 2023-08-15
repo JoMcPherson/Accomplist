91097da7342ebbc8b672e14807e543d62451647f
Joanna McPherson <mcphersonjoanna@gmail.com>
2023-08-14 20:28:47 -0700

    linting error fix

--- api/queries/accomplist_items.py
+++ api/queries/accomplist_items.py
@@ -30,23 +30,27 @@ class AccomplistItemOut(BaseModel):
 
 
 class AccomplistItemRepository():
-    def accomplist_in_to_out(self, id: int, accomplist_item: AccomplistItemIn):
-              old_data = accomplist_item.dict()
-              return AccomplistItemOut(id=id, **old_data)
+    def accomplist_in_to_out(self, id: int,
+                             accomplist_item: AccomplistItemIn
+                             ) -> AccomplistItemOut:
+        old_data = accomplist_item.dict()
+        return AccomplistItemOut(id=id, **old_data)
 
-    def record_to_accomplist_item_out(self, record):
-              return  AccomplistItemOut(
-                            id=record[0],
-                            title=record[1],
-                            details=record[2],
-                            photo=record[3],
-                            resources=record[4],
-                            things_to_do=record[5],
-                            things_not_to_do=record[6],
-                            date_added=record[7],
-                        )
+    def record_to_accomplist_item_out(self, record) -> AccomplistItemOut:
+        return AccomplistItemOut(
+            id=record[0],
+            title=record[1],
+            details=record[2],
+            photo=record[3],
+            resources=record[4],
+            things_to_do=record[5],
+            things_not_to_do=record[6],
+            date_added=record[7]
+        )
 
-    def get_accomplist_item(self, accomplist_item_id: int) -> Optional[AccomplistItemOut]:
+    def get_accomplist_item(self,
+                            accomplist_item_id: int
+                            ) -> Optional[AccomplistItemOut]:
         try:
             # connect the database
             with pool.connection() as conn:
@@ -55,13 +59,13 @@ class AccomplistItemRepository():
                     result = cur.execute(
                         """
                         SELECT id,
-                        title,
-                        details,
-                        photo,
-                        resources,
-                        things_to_do,
-                        things_not_to_do,
-                        date_added
+                               title,
+                               details,
+                               photo,
+                               resources,
+                               things_to_do,
+                               things_not_to_do,
+                               date_added
                         FROM accomplist_items
                         WHERE id = %s
                         """,
@@ -73,7 +77,7 @@ class AccomplistItemRepository():
                     return self.record_to_accomplist_item_out(record)
         except Exception as e:
             print(e)
-            return {"message: Could not get that accomplist item"}
+            return None
 
     def delete(self, accomplist_item_id: int) -> bool:
         try:
@@ -83,7 +87,7 @@ class AccomplistItemRepository():
                 with conn.cursor() as cur:
                     result = cur.execute(
                         """
-                        DELETE from accomplist_items
+                        DELETE FROM accomplist_items
                         WHERE id = %s
                         """,
                         [accomplist_item_id]
@@ -95,7 +99,11 @@ class AccomplistItemRepository():
             print(e)
             return False
 
-    def update(self, accomplist_item_id: int, accomplist_item: AccomplistItemIn) -> Union[AccomplistItemOut, Error]:
+    def update(
+            self,
+            accomplist_item_id: int,
+            accomplist_item: AccomplistItemIn
+            ) -> Union[Error, AccomplistItemOut]:
         try:
             # connect the database
             with pool.connection() as conn:
@@ -103,16 +111,15 @@ class AccomplistItemRepository():
                 with conn.cursor() as cur:
                     cur.execute(
                         """
-                            UPDATE accomplist_items
-                            SET title = %s,
+                        UPDATE accomplist_items
+                        SET title = %s,
                             details = %s,
                             photo = %s,
                             resources = %s,
                             things_to_do = %s,
                             things_not_to_do = %s,
                             date_added = %s
-                            WHERE id = %s
-
+                        WHERE id = %s
                         """,
                         [
                             accomplist_item.title,
@@ -125,12 +132,12 @@ class AccomplistItemRepository():
                             accomplist_item_id
                         ]
                     )
-
-                    return self.accomplist_in_to_out(accomplist_item_id,accomplist_item)
-
+                    return self.accomplist_in_to_out(
+                        accomplist_item_id, accomplist_item
+                        )
         except Exception as e:
             print(e)
-            return {"message": "could not update accomplist item"}
+            return Error(message="could not update accomplist item")
 
     def get_all(self) -> Union[Error, List[AccomplistItemOut]]:
         try:
@@ -154,42 +161,47 @@ class AccomplistItemRepository():
                         """
                     )
                     return [
-                        self.record_to_accomplist_item_out(record) for record in cur
+                        self.record_to_accomplist_item_out(record)
+                        for record in cur
                     ]
         except Exception as e:
             print(e)
-            return {"message": "could not get all accomplist items"}
+            return Error(message="could not get all accomplist items")
 
     def create(self, accomplist_item: AccomplistItemIn) -> AccomplistItemOut:
-        # connect the database
-        with pool.connection() as conn:
-            # get a cursor
-            with conn.cursor() as cur:
-                # run our INSERT statement
-                result = cur.execute(
-                    """
-                    INSERT INTO accomplist_items
+        try:
+            # connect the database
+            with pool.connection() as conn:
+                # get a cursor
+                with conn.cursor() as cur:
+                    # run our INSERT statement
+                    result = cur.execute(
+                        """
+                        INSERT INTO accomplist_items
                             (title,
-                            details,
-                            photo,
-                            resources,
-                            things_to_do,
-                            things_not_to_do,
-                            date_added)
-                    VALUES
-                    (%s, %s, %s, %s, %s, %s, %s)
-                    RETURNING id;
-                    """,
-                    [
-                        accomplist_item.title,
-                        accomplist_item.details,
-                        accomplist_item.photo,
-                        accomplist_item.resources,
-                        accomplist_item.things_to_do,
-                        accomplist_item.things_not_to_do,
-                        accomplist_item.date_added,
-                    ],
-                )
-                id = result.fetchone()[0]
-                # Return new data
-                return self.accomplist_in_to_out(id,accomplist_item)
+                             details,
+                             photo,
+                             resources,
+                             things_to_do,
+                             things_not_to_do,
+                             date_added)
+                        VALUES
+                            (%s, %s, %s, %s, %s, %s, %s)
+                        RETURNING id;
+                        """,
+                        [
+                            accomplist_item.title,
+                            accomplist_item.details,
+                            accomplist_item.photo,
+                            accomplist_item.resources,
+                            accomplist_item.things_to_do,
+                            accomplist_item.things_not_to_do,
+                            accomplist_item.date_added,
+                        ],
+                    )
+                    id = result.fetchone()[0]
+                    # Return new data
+                    return self.accomplist_in_to_out(id, accomplist_item)
+        except Exception as e:
+            print(e)
+            return Error(message="could not create accomplist item")
--- api/routers/accomplist_items.py
+++ api/routers/accomplist_items.py
@@ -10,28 +10,31 @@ from queries.accomplist_items import (
 router = APIRouter()
 
 
-@router.post("/accomplist_items",
-             response_model=AccomplistItemOut)
+@router.post("/accomplist_items", response_model=AccomplistItemOut)
 def create_accomplist_item(
     accomplist_item: AccomplistItemIn,
     repo: AccomplistItemRepository = Depends(),
-):
+) -> AccomplistItemOut:
     return repo.create(accomplist_item)
 
 
 @router.get("/accomplist_items",
             response_model=Union[List[AccomplistItemOut], Error])
-def get_all(repo: AccomplistItemRepository = Depends()):
+def get_all(
+        repo: AccomplistItemRepository = Depends()
+        ) -> Union[List[AccomplistItemOut], Error]:
     return repo.get_all()
 
 
-@router.put("/accomplist_items/{accomplist_item_id}", response_model=Union[Error,AccomplistItemOut])
+@router.put("/accomplist_items/{accomplist_item_id}",
+            response_model=Union[Error, AccomplistItemOut])
 def update_accomplist_item(
     accomplist_item_id: int,
     accomplist_item: AccomplistItemIn,
     repo: AccomplistItemRepository = Depends(),
-) -> Union[Error,AccomplistItemOut]:
+) -> Union[Error, AccomplistItemOut]:
     return repo.update(accomplist_item_id, accomplist_item)
+
 
 @router.delete("/accomplist_items/{accomplist_item_id}", response_model=bool)
 def delete_accomplist_item(
@@ -40,12 +43,14 @@ def delete_accomplist_item(
 ) -> bool:
     return repo.delete(accomplist_item_id)
 
-@router.get("/accomplist_items/{accomplist_item_id}", response_model=Optional[AccomplistItemOut])
+
+@router.get("/accomplist_items/{accomplist_item_id}",
+            response_model=Optional[AccomplistItemOut])
 def get_accomplist_item(
     accomplist_item_id: int,
     response: Response,
     repo: AccomplistItemRepository = Depends(),
-) -> AccomplistItemOut:
+) -> Optional[AccomplistItemOut]:
     accomplist_item = repo.get_accomplist_item(accomplist_item_id)
     if accomplist_item is None:
         response.status_code = 404
