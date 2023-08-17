30516cab9ceb1a4efd602e2a7433adf8606e92e3
Joanna McPherson <mcphersonjoanna@gmail.com>
2023-08-17 15:55:22 -0700

    updated tables name in anticipation

--- api/migrations/005_my_accomplist_items.py
+++ api/migrations/005_my_accomplist_items.py
@@ -5,7 +5,7 @@ steps = [
         CREATE TABLE my_accomplist_items (
             id SERIAL PRIMARY KEY NOT NULL,
             item_id INT NOT NULL REFERENCES accomplist_items(id),
-            user_id INT NOT NULL REFERENCES users(id),
+            user_id INT NOT NULL REFERENCES users_accounts(id),
             completed BOOL NOT NULL
         );
         """,
