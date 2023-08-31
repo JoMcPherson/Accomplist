e6d905ef0a7288c66dc5ae70f07508ecb8dee1f6
Joanna <mcphersonjoanna@gmail.com>
2023-08-31 20:58:38 +0000

    Accomplist item create unit test

---
+++ api/tests/test_accomplist_items.py
@@ -0,0 +1,56 @@
+from fastapi.testclient import TestClient
+from queries.accomplist_items import AccomplistItemRepository
+from main import app
+
+client = TestClient(app)
+
+
+class CreateAccomplistItemRepo:
+    def create(self, accomplist_item):
+        result = {
+            "id": 1010,
+            "user_id": 0,
+            "title": "Foo Bar",
+            "details": "The Foo Barters",
+            "photo": "foobar.jpg",
+            "resources": "fooing.com",
+            "things_to_do": "foo foo",
+            "things_not_to_do": "bar bar",
+            "date_added": "2023-08-29",
+        }
+        result.update(accomplist_item)
+        return result
+
+
+def test_create_item():
+    app.dependency_overrides[
+        AccomplistItemRepository
+    ] = CreateAccomplistItemRepo
+
+    json = {
+        "user_id": 0,
+        "title": "Foo Bar",
+        "details": "The Foo Barters",
+        "photo": "foobar.jpg",
+        "resources": "fooing.com",
+        "things_to_do": "foo foo",
+        "things_not_to_do": "bar bar",
+        "date_added": "2023-08-29",
+    }
+
+    response = client.post("/api/accomplist_items/", json=json)
+    app.dependency_overrides = {}
+
+    print(response.json(), "response")
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": 1010,
+        "user_id": 0,
+        "title": "Foo Bar",
+        "details": "The Foo Barters",
+        "photo": "foobar.jpg",
+        "resources": "fooing.com",
+        "things_to_do": "foo foo",
+        "things_not_to_do": "bar bar",
+        "date_added": "2023-08-29",
+    }
