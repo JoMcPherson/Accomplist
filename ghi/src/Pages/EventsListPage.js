008c94a52ed85a1445cf0d44d14447849ac5fe37
Christopher Byrne <bt_byrne@yahoo.com>
2023-09-08 00:15:27 +0000

    Cosmetic Edit

--- ghi/src/Pages/EventsListPage.js
+++ ghi/src/Pages/EventsListPage.js
@@ -85,6 +85,62 @@ export default function EventsList() {
             <button className={eventstyles.button49} onClick={handleNewEventCreationPlace}>Create Event</button>
         </div>
 
+
+        <div className="flex h-screen w-screen flex-col justify-start overflow-hidden">
+      <div className="mt-16 mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+        <div>
+          <h1 className="text-center text-2xl font-bold sm:text-3xl gradient-text">
+            Neighborhoods
+          </h1>
         </div>
+        <div className="mx-auto max-w-lg">
+          <div className="relative bg-white px-6 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10 mt-4">
+            <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+              <thead className="bg-gray-50 dark:bg-gray-800">
+                <tr>
+                  <th className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    Neighborhood
+                  </th>
+                  <th className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    Postal Codes
+                  </th>
+                  {token && <th className="px-6 py-3 bg-gray-50"></th>}
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
+                {eventListData.map(event => {
+                    return (
+                      <tr key={event.event_id}>
+                        <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
+                          <div className="inline-flex items-center gap-x-3">
+                            {event.name}
+                          </div>
+                        </td>
+                        <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
+                          <div className="inline-flex items-center gap-x-3">
+                            {event.location}
+                          </div>
+                        </td>
+                      </tr>
+                    );
+                  }
+                )}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <div className="absolute bottom-0 left-0 right-0 overflow-hidden">
+        <div className="flex justify-around">
+
+        </div>
+      </div>
+    </div>
+
+
+
+        </div>
+
+
     )
 }
