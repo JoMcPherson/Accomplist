40b1603c6ddcbcfe75ad9ea1cd37e858ff4b74ff
Michael Hogan <antismart@gmail.com>
2023-09-14 11:58:54 -0700

    navbar final

--- ghi/src/Components/NavBar.js
+++ ghi/src/Components/NavBar.js
@@ -37,14 +37,14 @@ const CustomNavbar = () => {
       }
     };
 
-    const handleDocumentClick = (event) => {
-        if (navbarRef.current && !navbarRef.current.contains(event.target)) {
-          const navbarToggler = document.querySelector(".navbar-toggler");
-          if (navbarToggler && !navbarToggler.classList.contains("collapsed")) {
-            navbarToggler.click();
-          }
+  const handleDocumentClick = (event) => {
+      if (navbarRef.current && !navbarRef.current.contains(event.target)) {
+        const navbarToggler = document.querySelector(".navbar-toggler");
+        if (navbarToggler && !navbarToggler.classList.contains("collapsed")) {
+          navbarToggler.click();
         }
-      };
+      }
+    };
 
     window.addEventListener("scroll", handleScroll);
     document.addEventListener("click", handleDocumentClick);
@@ -62,30 +62,31 @@ const CustomNavbar = () => {
           Accomplist
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="exampleNavComponents" />
-        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-        <Nav className="ml-auto">
-          <NavDropdown title="Accomplist Items" id="basic-nav-dropdown" className="custom-dropdown">
-            <NavDropdown.Item as={Link} to="/accomplist_items/new">Create an Accomplist Item</NavDropdown.Item>
-            <NavDropdown.Item as={Link} to="/accomplist_items">View all Accomplist Items</NavDropdown.Item>
-            <NavDropdown.Item as={Link} to="/my_accomplist_items/new">Add to my Accomplist Items</NavDropdown.Item>
-        </NavDropdown>
-        <NavDropdown title="Events" id="basic-nav-dropdown" className="custom-dropdown">
-          <NavDropdown.Item as={Link} to="/events">Events</NavDropdown.Item>
-          <NavDropdown.Item as={Link} to="/my_events/new">Add to My Events</NavDropdown.Item>
-        </NavDropdown>
-        {token ? ( <NavItem className="px-2"><Nav.Link as={Link} to="/profile">My Profile</Nav.Link></NavItem>) : null}
-        {token ? (
-          <NavItem className="px-2"><NavLink onClick={handleLogout}>Logout</NavLink></NavItem>
-            ) : (
-          <NavItem className="px-3"><NavLink as={Link} to="/Login">Login</NavLink></NavItem>
-            )}
-        </Nav>
-        <Form className="navbar-form navbar-right px-2" onSubmit={handleSearchSubmit}>
-        <FormControl type="text" placeholder="Search" value={inputValue} onChange={handleSearchChange}/>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <NavDropdown title="Accomplist Items" id="basic-navbar-nav">
+              <NavDropdown.Item as={Link} to="/accomplist_items">View all Accomplist items</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/accomplist_items/new">Create an Accomplist item</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/my_accomplist_items/new">Add to my Accomplist items</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Events" id="basic-navbar-nav">
+              <NavDropdown.Item as={Link} to="/events">View all events</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/events">Create an event</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/my_events/new">Add to my events</NavDropdown.Item>
+            </NavDropdown>
+            { token ? (<>
+              <NavItem><NavLink as={Link} to="/profile">My Profile</NavLink></NavItem>
+              <NavItem><NavLink onClick={handleLogout}>Logout</NavLink></NavItem>
+              </>) : (
+              <NavItem><NavLink as={Link} to="/login">Login</NavLink></NavItem>)}
+          </Nav>
+        <Form className="navbar-form navbar-right" onSubmit={handleSearchSubmit}>
+        <FormControl type="text" placeholder="Search" name="accomplistsearch" id="search" value={inputValue} onChange={handleSearchChange}/>
           </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 };
+
 export default CustomNavbar;
